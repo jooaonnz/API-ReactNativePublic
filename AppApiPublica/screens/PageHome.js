5c93db3cbbbcf9add1bca3c5249e3343ffb18b25
JOÃO EDUARDO MARTINS ESKELSEN <joao_eskelsen@estudante.sesisenai.org.br>
2025-10-06 14:16:53 -0300

    Adicionado comentarios na PageHome

--- AppApiPublica/screens/PageHome.js
+++ AppApiPublica/screens/PageHome.js
@@ -14,3 +14,11 @@ const styles = StyleSheet.create()
 {
 
 }
+/*
+ Api pública de câmbio
+    https://www.exchangerate-api.com/docs/free?ref=freepublicapis.com
+
+ Site com apis disponível
+    https://www.freepublicapis.com/tags/public-data
+
+*/
