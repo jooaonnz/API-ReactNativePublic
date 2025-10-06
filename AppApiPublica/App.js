54f20d8ebba7fc2c838c81b6a9787d0bd62023f4
JOÃO EDUARDO MARTINS ESKELSEN <joao_eskelsen@estudante.sesisenai.org.br>
2025-10-06 14:15:22 -0300

    PageHome criada

--- AppApiPublica/App.js
+++ AppApiPublica/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import {PageHome} from "./screens/PageHome"
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="PageHome">
+          <Stack.Screen
+          name="PageHome"
+          component={PageHome}
+          options={{headerShown:false}}
+          />
+      
+        </Stack.Navigator>
+        
+      </NavigationContainer>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ AppApiPublica/screens/PageHome.js
@@ -0,0 +1,16 @@
+import {Text,View, Button, StyleSheet} from "react";
+
+
+export default function PageHome(navigation){
+    return( 
+        <View style={Styles.Text}>
+
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create()
+{
+
+}
